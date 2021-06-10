bd338d2cb1fcb8f1fa614101bc0a9e30726edcdd
Matt Lynam <mlynam@iteamnm.com>
2021-06-10 12:49:22 -0600

    feat: make the nav layout aware

--- src/components/Nav/Nav.stories.tsx
+++ src/components/Nav/Nav.stories.tsx
@@ -4,6 +4,8 @@ import { Link, Route, Router, Switch } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import NavItem from './NavItem';
 import NavItemList from './NavItemList';
+import Layout from '../Layout/Layout';
+import Main from '../Main/Main';
 
 export default {
   title: 'Components/Nav',
@@ -16,39 +18,50 @@ const history = createMemoryHistory();
 const Template: Story<StoryProps> = args => {
   return (
     <Router history={history}>
-      <Nav {...args} history={history}>
-        <NavItem>
-          <Link to="/page-1">Page 1</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/page-2">Page 2</Link>
-        </NavItem>
-        <NavItemList title="More stuff" path="/more">
-          <NavItem>
-            <Link to="/more/page-1">Page 1</Link>
-          </NavItem>
-          <NavItem>
-            <Link to="/more/page-2">Page 2</Link>
-          </NavItem>
-        </NavItemList>
-      </Nav>
-      <Switch>
-        <Route path="/page-1">
-          <span>Hello from Page 1</span>
-        </Route>
-        <Route path="/page-2">
-          <span>Hello from Page 2</span>
-        </Route>
-        <Route path="/more/page-1">
-          <span>Hello from more Page 1</span>
-        </Route>
-        <Route path="/more/page-2">
-          <span>Hello from more Page 2</span>
-        </Route>
-        <Route>
-          <span>Not found</span>
-        </Route>
-      </Switch>
+      <Layout mode="flex">
+        <Layout style={{ width: 225 }}>
+          <Nav {...args} history={history}>
+            <NavItem>
+              <Link to="/page-1">Page 1</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/page-2">Page 2</Link>
+            </NavItem>
+            <NavItemList title="More stuff" path="/more">
+              <NavItem>
+                <Link to="/more/page-1">Page 1</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/more/page-2">Page 2</Link>
+              </NavItem>
+            </NavItemList>
+          </Nav>
+        </Layout>
+        <Layout mode="flex" fill orientation="y">
+          <Layout spacing="xl">Search | ...</Layout>
+          <Layout mode="flex" fill spacing="xl">
+            <Main>
+              <Switch>
+                <Route path="/page-1">
+                  <span>Hello from Page 1</span>
+                </Route>
+                <Route path="/page-2">
+                  <span>Hello from Page 2</span>
+                </Route>
+                <Route path="/more/page-1">
+                  <span>Hello from more Page 1</span>
+                </Route>
+                <Route path="/more/page-2">
+                  <span>Hello from more Page 2</span>
+                </Route>
+                <Route>
+                  <span>Not found</span>
+                </Route>
+              </Switch>
+            </Main>
+          </Layout>
+        </Layout>
+      </Layout>
     </Router>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -7,6 +7,7 @@ export { default as AccordionProvider } from './Accordion/AccordionProvider';
 export { default as Accordion } from './Accordion/Accordion';
 export { default as Button } from './Button/Button';
 export { default as Clickout } from './Popover/Popover';
+export { default as Layout } from './Layout/Layout';
 export { default as Main } from './Main/Main';
 export { default as Menu } from './Menu/Menu';
 export { default as Nav } from './Nav/Nav';
@@ -16,6 +17,7 @@ export { default as usePopover } from './Popover/usePopover';
 
 export type { ButtonProps } from './Button/Button';
 export type { ClickoutProps } from './Clickout/Clickout';
+export type { LayoutProps } from './Layout/Layout';
 export type { MainProps } from './Main/Main';
 export type { MenuProps } from './Menu/Menu';
 export type { NavProps } from './Nav/Nav';
